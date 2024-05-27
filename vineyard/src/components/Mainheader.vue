63f8faf9c8f4533bcbaa2a2504a25046cf833c25
mitaaaa803 <a0956880803@gmail.com>
2024-05-27 11:40:17 +0800

    header

--- vineyard/src/components/Mainheader.vue
+++ vineyard/src/components/Mainheader.vue
@@ -1,3 +1,38 @@
 <template>
-  <div>123</div>
+  <header class="header">
+    <div class="wrapper">
+
+      <div class="header hamburger">
+        <i class="fa-solid fa-bars"></i>
+      </div>
+
+      <div class="header logo">
+        <img src="https://fakeimg.pl/50x50/200">
+      </div>
+
+
+      <ul class="header link">
+        <li>
+          <a href="#">我們的酒</a>
+          <a href="#">品酒課程</a>
+          <a href="#">最新消息</a>
+          <a href="#">關於我們</a>
+        </li>    
+      </ul>
+
+      <div class="wrap_login_shoppingcart">
+        <div class="header login">
+          <i class="fa-regular fa-user"></i>
+          <a href="#">登入</a>
+        </div>
+
+        <div class="header shoppingcart">
+          <i class="fa-solid fa-bag-shopping"></i>
+          <a href="#">詢價清單</a>
+        </div>
+
+      </div>
+    </div>
+    
+  </header>
 </template>
