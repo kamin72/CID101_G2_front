af86ac41126f40815d4dfde34ca708ef0d2117a7
mitaaaa803 <a0956880803@gmail.com>
2024-05-28 11:53:56 +0800

    footer 的 p 標籤改成small 標籤

--- vineyard/src/components/Mainfooter.vue
+++ vineyard/src/components/Mainfooter.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="footer">
-    <div class="footer_wrapper">
+    <nav class="container">
       <div class="footer_logo">
         <a href="#"><img src="/img/logo/logo2-w.png" alt="" /></a>
       </div>
@@ -19,13 +19,13 @@
           <a>品酒課程</a>
         </div>
       </div>
-    </div>
+    </nav>
   </footer>
   <div class="tibame">
-    <p>本網站為緯育TibaMe_前端設計工程師班第88期學員專題成果作品,本平台僅供學習、展示之用。</p>
-    <p>
+    <small>本網站為緯育TibaMe_前端設計工程師班第88期學員專題成果作品,本平台僅供學習、展示之用。</small>
+    <small>
       若有抵觸有關著作權,或有第三人主張侵害智慧財產權等情事,均由學員負法律上責任,緯育公司概不負責。
-    </p>
-    <p>若有侵權疑慮,您可以私訊緯育TibaMe,後續會由專人協助處理。</p>
+    </small>
+    <small>若有侵權疑慮,您可以私訊緯育TibaMe,後續會由專人協助處理。</small>
   </div>
 </template>
--- vineyard/src/components/Mainheader.vue
+++ vineyard/src/components/Mainheader.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="header">
-    <div class="header_wrapper">
+    <nav class="container">
       <div class="header_hamburger">
         <i class="fa-solid fa-bars"></i>
       </div>
@@ -13,9 +13,9 @@
         <li>
           <a href="#">我們的酒</a>
           <a href="#">品酒課程</a>
+          <a href="#">品酒達人</a>
           <a href="#">最新消息</a>
           <a href="#">關於我們</a>
-          <a href="#">品酒達人</a>
         </li>
       </ul>
 
@@ -30,6 +30,6 @@
           <a href="#">詢價清單(0)</a>
         </div>
       </div>
-    </div>
+    </nav>
   </header>
 </template>
