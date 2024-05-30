5d690c9db80f21b10cf72d3f7a16a2b48af962fb
kamin72 <a1616001@gmail.com>
2024-05-30 20:57:37 +0800

    修改卡片2圖片滿版、新增reset.css/scss

--- vineyard/src/components/Cards.vue
+++ vineyard/src/components/Cards.vue
@@ -1,7 +1,30 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <!-- <div class="col-3 col-md-3 col-sm-6 event-card1">
+  <!-- 活動卡片1 -->
+  <div class="container">
+    <div class="row">
+      <div class="col-3 col-md-3 col-sm-6 event-card1">
+        <div class="img-wrap">
+          <img src="https://picsum.photos/300/300/?random=10" />
+          <div class="card-ribbon">
+            <small>早鳥優惠</small>
+          </div>
+          <div class="book-now">
+            <p>BOOK NOW</p>
+          </div>
+        </div>
+        <div class="font-wrap">
+          <h4>品酒初級課程1</h4>
+          <p>2024/5/18 開課</p>
+        </div>
+        <h4>NT. 3,200</h4>
+      </div>
+    </div>
+  </div>
+  <!-- 活動卡片2 -->
+  <div class="container">
+    <div class="row">
+      <div class="col-6">
+        <!-- <div class="col-3 col-md-3 col-sm-6 event-card1">
                 <div class="img-wrap">
                     <img src="https://picsum.photos/300/300/?random=10" />
                     <div class="card-ribbon">
@@ -17,25 +40,25 @@
                 </div>
                 <h4>NT. 3,200</h4>
             </div> -->
-            <div class="col-6 event-card2">
-                <div class="img-wrap">
-                    <img src="https://picsum.photos/600/600/?random=10" />
-                </div>
-                <div class="info-wrap">
-                    <div class="left-wrap">
-                        <h3>class</h3>
-                        <h4>class date</h4>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-                    </div>
-                    <div class="right-wrap">
-                        <div class="price">
-                            <h4>NT. 3200</h4>
-                        </div>
-                    </div>
-                </div>
-                <div class="big-btn-primary">詳情資訊</div>
+        <div class="event-card2">
+          <div class="img-wrap">
+            <img src="https://picsum.photos/600/600/?random=10" />
+          </div>
+          <div class="info-wrap">
+            <div class="left-wrap">
+              <h3>class</h3>
+              <h4>class date</h4>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
             </div>
-            <!-- <div class="col-3 col-md-3 col-sm-6 product-card">
+            <div class="right-wrap">
+              <div class="price">
+                <h4>NT. 3200</h4>
+              </div>
+            </div>
+          </div>
+          <div class="big-btn-primary">詳情資訊</div>
+        </div>
+        <!-- <div class="col-3 col-md-3 col-sm-6 product-card">
                 <div class="img-wrap">
                     <img src="https://picsum.photos/300/200/?random=10">
                 </div>
@@ -51,7 +74,28 @@
                     <p>加入詢價單</p>
                 </div>
             </div> -->
-            
-        </div>
+      </div>
     </div>
+  </div>
+  <!-- 活動卡片3 -->
+  <div class="container">
+    <div class="row">
+      <div class="col-3 col-md-3 col-sm-6 product-card">
+        <div class="img-wrap">
+          <img src="https://picsum.photos/300/200/?random=10" />
+        </div>
+        <div class="info-wrap">
+          <div class="font-wrap">
+            <h4>法國紅酒</h4>
+            <p>Chenin-Blanc 白梢楠</p>
+            <p>1997年</p>
+          </div>
+          <h4>NT. 2,500</h4>
+        </div>
+        <div class="add-card">
+          <p>加入詢價單</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
