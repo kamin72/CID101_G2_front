b77b99b9eb196c06d6625f4668c3c8d8d271f7a7
l11y0 <leeyo.yh@gmail.com>
2024-05-31 18:28:57 +0800

    修正卡片語法

--- vineyard/src/components/Cards.vue
+++ vineyard/src/components/Cards.vue
@@ -20,108 +20,35 @@
       </div>
     </div>
   </div>
+
   <!-- 活動卡片2 -->
   <div class="container">
     <div class="row">
       <div class="col-6">
-        <!-- <div class="col-3 col-md-3 col-sm-6 event-card1">
-                <div class="img-wrap">
-                    <img src="https://picsum.photos/300/300/?random=10" />
-                    <div class="card-ribbon">
-                        <small>早鳥優惠</small>
-                    </div>
-                    <div class="book-now">
-                        <p>BOOK NOW</p>
-                    </div>
-                </div>
-                <div class="font-wrap">
-                    <h4>品酒初級課程1</h4>
-                    <p>2024/5/18 開課</p>
-                </div>
-                <h4>NT. 3,200</h4>
-            </div> -->
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> dev
-
-            <div class="event-card2">
-                <div class="img-wrap">
-                    <img src="https://picsum.photos/600/600/?random=10" />
-                </div>
-                <div class="info-wrap">
-                    <div class="left-wrap">
-                        <div class="title-wrap">
-                            <h4>初級品酒課程1</h4>
-                            <p>2024/05/18 開課</p>
-                        </div>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-                    </div>
-                    <div class="right-wrap">
-                        <div class="price">
-                            <h4>NT.3200</h4>
-                        </div>
-                    </div>
-                </div>
-                <div class="small-btn-primary">詳情資訊</div>
-            </div>
-
-            <!-- <div class="col-3 col-md-3 col-sm-6 product-card">
-=======
         <div class="event-card2">
           <div class="img-wrap">
             <img src="https://picsum.photos/600/600/?random=10" />
           </div>
           <div class="info-wrap">
             <div class="left-wrap">
-              <h3>class</h3>
-              <h4>class date</h4>
+              <div class="title-wrap">
+                <h4>初級品酒課程1</h4>
+                <p>2024/05/18 開課</p>
+              </div>
               <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
             </div>
-=======
-        <div class="event-card2">
-          <div class="img-wrap">
-            <img src="https://picsum.photos/600/600/?random=10" />
-          </div>
-          <div class="info-wrap">
-            <div class="left-wrap">
-              <h3>class</h3>
-              <h4>class date</h4>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-            </div>
->>>>>>> 86859cdc49012a6c50b53f0c69ca50bee93aa8bc
             <div class="right-wrap">
               <div class="price">
                 <h4>NT. 3200</h4>
               </div>
             </div>
           </div>
-          <div class="big-btn-primary">詳情資訊</div>
+          <div class="small-btn-primary">詳情資訊</div>
         </div>
-        <!-- <div class="col-3 col-md-3 col-sm-6 product-card">
-<<<<<<< HEAD
->>>>>>> 86859cdc49012a6c50b53f0c69ca50bee93aa8bc
-=======
->>>>>>> 86859cdc49012a6c50b53f0c69ca50bee93aa8bc
-                <div class="img-wrap">
-                    <img src="https://picsum.photos/300/200/?random=10">
-                </div>
-                <div class="info-wrap">
-                    <div class="font-wrap">
-                        <h4>法國紅酒</h4>
-                        <p>Chenin-Blanc 白梢楠</p>
-                        <p>1997年</p>
-                    </div>
-                    <h4>NT. 2,500</h4>
-                </div>
-                <div class="add-card">
-                    <p>加入詢價單</p>
-                </div>
-            </div> -->
       </div>
     </div>
   </div>
+
   <!-- 活動卡片3 -->
   <div class="container">
     <div class="row">
