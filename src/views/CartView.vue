052e95532ae6811b53c243897fb85b5a59877140
kamin72 <a1616001@gmail.com>
2024-06-04 11:43:11 +0800

    購物流程架構及CSS修改

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,8 +1,9 @@
 <template>
 
   <div class="container">
-    <CartFlow />
-
+    <section>
+      <CartFlow :flow="item" v-for="item in flow" :key="item.id" />
+    </section>
     <div class="wrap_all">
       <div class="wrap_list">
         <div class="textItem">
@@ -31,13 +32,6 @@
       </div>
       <Coupon />
     </div>
-
-
-
-
-
-
-
   </div>
 
 </template>
@@ -84,7 +78,37 @@ export default {
           price: 600,
           count: 1
         }
-      ]
+      ],
+      flow: [
+        {
+          id: 1,
+          icon: 'receipt_long',
+          opacity: '1',
+          text: '詢價清單',
+          bold: '400'
+        },
+        {
+          id: 2,
+          icon: 'local_shipping',
+          opacity: '0.3',
+          text: '填寫配送資訊',
+          bold: '0'
+        },
+        {
+          id: 3,
+          icon: 'paid',
+          opacity: '0.3',
+          text: '選擇付款方式',
+          bold: '0'
+        },
+        {
+          id: 4,
+          icon: 'check',
+          opacity: '0.3',
+          text: '完成詢價',
+          bold: '0'
+        }
+      ],
     }
   },
   methods: {
