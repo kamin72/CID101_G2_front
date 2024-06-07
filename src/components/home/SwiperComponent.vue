051a361bfdf2d661809b0f06ab6ccf086df724cb
purplekuo <ang840725@gmail.com>
2024-06-07 20:12:59 +0800

    修改首頁輪播卡片

--- src/components/home/SwiperComponent.vue
+++ src/components/home/SwiperComponent.vue
@@ -16,21 +16,21 @@
             {
                 id:1,
                 title: "品酒體驗-初級課程1",
-                price:'$1200',
+                price:'1200',
                 date:'2024/5/18 開課',
                 img:'/src/assets/img/home/eventcard1.png'
             },
             {
                 id:2,
                 title: "品酒體驗-初級課程1",
-                price:'$1200',
+                price:'1200',
                 date:'2024/5/18 開課',
                 img:'/src/assets/img/home/eventcard2.png'
             },
             {
                 id:3,
                 title: "品酒體驗-中級課程1",
-                price:'$1200',
+                price:'1200',
                 date:'2024/5/18 開課',
                 img:'/src/assets/img/home/eventcard3.png',
                 tag:'早鳥優惠'
@@ -38,14 +38,14 @@
             {
                 id:4,
                 title: "品酒體驗-進階課程1",
-                price:'$1200',
+                price:'1200',
                 date:'2024/5/18 開課',
                 img:'/src/assets/img/home/eventcard4.png'
             },
             {
                 id:5,
                 title: "品酒體驗-初級課程2",
-                price:'$1200',
+                price:'1200',
                 date:'2024/5/18 開課',
                 img:'/src/assets/img/home/eventcard5.png'
             }
@@ -79,17 +79,20 @@
 
     <swiper-slide v-for="(item) in content" :key="item.id">
       
-        <div class="swiper-image">
-        <img :src="item.img" alt="item.title">
-        <div class="tag">
-            <p>{{item.tag}}</p>
+        <div class="img-wrap">
+            <img :src="item.img" alt="item.title">
+            <div class="card-ribbon">
+                <p>{{item.tag}}</p>
+            </div>
+            <a class="book-now" href="#">BOOK NOW</a>
         </div>
-        <a class="book" href="#">BOOK NOW</a>
+        <div class="font-wrap">
+            <h4>{{ item.title }}</h4>
+            <p>{{ item.date }}</p>
         </div>
-        
-        <h5>{{ item.title }}</h5>
-        <p>{{ item.price }}</p>
-        <p>{{ item.date }}</p>
+        <div class="price">
+            <h4><span>NT. </span>{{ item.price }}</h4>
+        </div>
     </swiper-slide>
 </Swiper>
 </template>
