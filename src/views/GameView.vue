6c8dcbe59771a44d6ce107f364bb245f39a65930
mitaaaa803 <a0956880803@gmail.com>
2024-06-13 08:47:42 +0800

    遊戲頁

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,3 +1,11 @@
 <template>
-  <div>game</div>
+  <section class="game">
+    <div class="game-container">
+      <div class="game-txt">
+        <h3>Wine Quiz</h3>
+        <p>品酒達人</p>
+      </div>
+      <button>GO!</button>
+    </div>
+  </section>
 </template>
