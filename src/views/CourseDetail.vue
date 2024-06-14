da72bcf9335b7afbe3776e5b7a84616d70bd1b2e
l11y0 <leeyo.yh@gmail.com>
2024-06-14 20:32:04 +0800

    修改課程詳細頁面

--- src/views/CourseDetail.vue
+++ src/views/CourseDetail.vue
@@ -1,11 +1,18 @@
 <template>
   <!-- 大圖banner -->
-  <section class="courseBanner"></section>
+  <div class="courseBanner">
+    <div class="bgImgWrap">
+      <img src="../assets/img/course/courseDetail/bgImg.jpg" alt="" />
+    </div>
+    <div class="maskWrap">
+      <img src="../assets/img/home/homebanner2.png" alt="" />
+    </div>
+  </div>
   <!-- 課程資訊 -->
   <section class="info">
     <div class="container">
-      <div class="row">
-        <div class="col-12 courseTopInfoWrap">
+      <div class="courseDetailRow">
+        <div class="col-11 courseTopInfoWrap">
           <div class="courseInfo">
             <h3>品酒初級課程1</h3>
             <div class="courseDetailWrap">
@@ -45,80 +52,87 @@
       </div>
     </div>
   </section>
-  <section>
-    <div class="container">
-      <div class="courseDetailDivider"></div>
-    </div>
-  </section>
   <!-- 課程介紹 -->
   <section class="introduction">
     <div class="container">
-      <div class="row">
-        <div class="teacher">
-          <div class="teacherImg">
-            <img
-              src="/src/assets/img/course/Firefly 酒莊裡品酒課的亞洲女老師的照片 10250 1.jpg"
-              alt="酒莊裡品酒課的亞洲女老師的照片"
-            />
+      <div class="courseDetailRow">
+        <div class="col-11 courseBottomInfoWrap">
+          <div class="teacher">
+            <div class="teacherImg">
+              <img
+                src="/src/assets/img/course/courseDetail/teacher.jpg"
+                alt="teacher"
+              />
+            </div>
+            <div class="teacherInfo">
+              <h3 class="teacherName">嚴鈺婷 Ms. Yan</h3>
+              <h4 class="expertise">資深品酒家 / WSET高級認證執酒師</h4>
+              <p class="teacherIntro">
+                嚴鈺婷出身釀酒世家，自幼對酒類深感興趣。她畢業於英國頂尖酒學院，後赴多國實習，累積15年釀酒與品酒經驗。嚴鈺婷精通葡萄酒釀造，是資深品酒家和鑑酒師，對葡萄酒細節有獨到見解。她擁有WSET高級認證，是國內頂尖品酒權威講師，上課風格專注幽默，致力傳承品酒樂趣，啟發更多人熱愛美酒。
+              </p>
+            </div>
           </div>
-          <div class="teacherInfo">
-            <h3 class="teacherName">嚴鈺婷 Ms. Yan</h3>
-            <h4 class="expertise">資深品酒家 / WSET高級認證執酒師</h4>
-            <p class="teacherIntro">
-              嚴鈺婷從小就對酒類產生濃厚興趣,追溯她的家族世代都是釀酒世家。她自英國頂尖酒學院畢業後,便奔赴世界各地參與釀酒實習,足跡遍及法國、義大利、南非、澳洲等著名產酒國。擁有超過15年釀酒和品酒經驗的她,不僅對葡萄酒的釀造流程瞭若指掌,更是位資深的品酒家和鑑酒師,對於各式葡萄酒的酒體、酒香、餘韻等細微特質有獨到的見解和解讀。曾取得WSET(英國葡萄酒暨烈酒教育基金會)高級認證的嚴鈺婷老師,品學兼優的專業背景令她成為國內頂尖的品酒權威講師,她專注紮實、風趣幽默的上課風格備受好評,期望能將品酒樂趣傳承並啟發更多人對美酒的熱愛。
-            </p>
+          <div class="feature">
+            <div class="featWrap">
+              <div class="iconWrap">
+                <img src="/src/assets/img/course/courseDetail/icon1.png" alt="" />
+              </div>
+              <h4>葡萄酒基礎知識</h4>
+              <ul>
+                <li>葡萄酒的歷史與分類</li>
+                <li>釀酒工藝和常見葡萄品種介紹</li>
+                <li>酒杯的選擇和基本禮儀</li>
+              </ul>
+            </div>
+            <div class="featWrap">
+              <div class="iconWrap">
+                <img src="/src/assets/img/course/courseDetail/icon2.png" alt="" />
+              </div>
+              <h4>視覺品酒</h4>
+              <ul>
+                <li>了解酒液顏色及其含義</li>
+                <li>辨識酒漬、酒體及其他視覺線索</li>
+                <li>實作:體驗不同顏色和質地的葡萄酒</li>
+              </ul>
+            </div>
+            <div class="featWrap">
+              <div class="iconWrap">
+                <img src="/src/assets/img/course/courseDetail/icon3.png" alt="" />
+              </div>
+              <h4>嗅覺品酒</h4>
+              <ul>
+                <li>認識不同酒香的來源</li>
+                <li>水果、香料、木質等香氣辨識</li>
+                <li>實作:培養嗅覺記憶與辨識能力</li>
+              </ul>
+            </div>
+            <div class="featWrap">
+              <div class="iconWrap">
+                <img src="/src/assets/img/course/courseDetail/icon4.png" alt="" />
+              </div>
+              <h4>口感品酒</h4>
+              <ul>
+                <li>酸度、單寧、甜度等味覺體驗</li>
+                <li>後味和餘韻的感受</li>
+                <li>實作:品嚐各種葡萄酒並作記錄</li>
+              </ul>
+            </div>
+            <div class="featWrap">
+              <div class="iconWrap">
+                <img src="/src/assets/img/course/courseDetail/icon5.png" alt="" />
+              </div>
+              <h4>醒酒與珍藏</h4>
+              <ul>
+                <li>了解醒酒的目的和技巧</li>
+                <li>適合醒酒的酒款和時間</li>
+                <li>家用酒窖葡萄酒存放建議</li>
+              </ul>
+            </div>
           </div>
+          <button class="big-btn-primary reserveCourse">
+            <span class="material-symbols-outlined"> edit_calendar </span>預約課程
+          </button>
         </div>
-        <div class="feature">
-          <div class="featWrap">
-            <img src="/src/assets/img/course/list 1.png" alt="" />
-            <h3>葡萄酒基礎知識</h3>
-            <ul>
-              <li>葡萄酒的歷史與分類</li>
-              <li>釀酒工藝和常見葡萄品種介紹</li>
-              <li>酒杯的選擇和基本禮儀</li>
-            </ul>
-          </div>
-          <div class="featWrap">
-            <img src="/src/assets/img/course/wine 2.png" alt="" />
-            <h3>視覺品酒</h3>
-            <ul>
-              <li>了解酒液顏色及其含義</li>
-              <li>辨識酒漬、酒體及其他視覺線索</li>
-              <li>實作:體驗不同顏色和質地的葡萄酒</li>
-            </ul>
-          </div>
-          <div class="featWrap">
-            <img src="/src/assets/img/course/wine-tasting (2) 1.png" alt="" />
-            <h3>嗅覺品酒</h3>
-            <ul>
-              <li>認識不同酒香的來源</li>
-              <li>水果、香料、木質等香氣辨識</li>
-              <li>實作:培養嗅覺記憶與辨識能力</li>
-            </ul>
-          </div>
-          <div class="featWrap">
-            <img src="/src/assets/img/course/wine-tasting.png" alt="" />
-            <h3>口感品酒</h3>
-            <ul>
-              <li>酸度、單寧、甜度等味覺體驗</li>
-              <li>後味和餘韻的感受</li>
-              <li>實作:品嚐各種葡萄酒並作記錄</li>
-            </ul>
-          </div>
-          <div class="featWrap">
-            <img src="/src/assets/img/course/wine-tasting (1).png" alt="" />
-            <h3>醒酒與珍藏</h3>
-            <ul>
-              <li>了解醒酒的目的和技巧</li>
-              <li>適合醒酒的酒款和時間</li>
-              <li>家用酒窖葡萄酒存放建議</li>
-            </ul>
-          </div>
-        </div>
-        <button class="big-btn-primary reserveCourse">
-          <span class="material-symbols-outlined"> edit_calendar </span>預約課程
-        </button>
       </div>
     </div>
   </section>
