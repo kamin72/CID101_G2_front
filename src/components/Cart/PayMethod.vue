4cc9e3b52a3ca8ccf1c6d29761db446a484f9483
kamin72 <a1616001@gmail.com>
2024-06-17 19:33:37 +0800

    付款方式選擇時背景顏色更換

--- src/components/Cart/PayMethod.vue
+++ src/components/Cart/PayMethod.vue
@@ -2,17 +2,17 @@
   <div class="box">
     <h4>付款方式</h4>
     <div class="methods">
-      <div class="creditCard">
+      <div class="creditCard" :class="{ clicked: selectedMethod == 0 }">
         <input type="radio" name="methods" @click="selectMethod(0)" :checked="checked == '0'" />
         <span class="material-symbols-outlined"> credit_card </span>
         <p>信用卡支付</p>
       </div>
-      <div class="store">
+      <div class="store" :class="{ clicked: selectedMethod == 1 }">
         <input type="radio" name="methods" @click="selectMethod(1)" :checked="checked == '1'" />
         <span class="material-symbols-outlined"> storefront </span>
         <p>7-11貨到付款</p>
       </div>
-      <div class="yardSelf">
+      <div class="yardSelf" :class="{ clicked: selectedMethod == 2 }">
         <input type="radio" name="methods" @click="selectMethod(2)" :checked="checked == '2'" />
         <span class="material-symbols-outlined"> liquor </span>
         <p>園區自取</p>
@@ -24,7 +24,9 @@
 <script>
 export default {
   data() {
-    return {}
+    return {
+      selectedMethod: null,
+    }
   },
   props: {
     checked: {
@@ -33,9 +35,13 @@ export default {
   },
   methods: {
     selectMethod(index) {
+      this.selectedMethod = index;
       this.$emit('change-method', index)
     }
   },
-  mounted() {}
+  mounted() {
+    // 初始化選中狀態
+    this.selectedMethod = parseInt(this.checked);
+  }
 }
 </script>
